b1cba13768b535e1de8da8e6f2b369074b5290b1
Gertjan De Wilde <info@gertjandewilde.com>
2017-01-20 20:35:30 +0100

    Set daily as default route

--- packages/nova-base-components/lib/routes.js
+++ packages/nova-base-components/lib/routes.js
@@ -1,6 +1,7 @@
 import { getComponent, addRoute } from 'meteor/nova:core';
 
 addRoute([
+<<<<<<< HEAD
   {name:'posts.list',     path: '/',                    component: getComponent('PostsHome')}, // index route
   {name:'posts.daily',    path:'daily',                 component: getComponent('PostsDaily')},
   {name:'posts.single',   path:'posts/:_id(/:slug)',    component: getComponent('PostsSingle')},
@@ -8,4 +9,13 @@ addRoute([
   {name:'users.account',  path:'account',               component: getComponent('UsersAccount')},
   {name:'resetPassword',  path:'reset-password/:token', component: getComponent('UsersResetPassword')},
   {name:'users.edit',     path:'users/:slug/edit',      component: getComponent('UsersAccount')},
+=======
+  {name:'posts.list',    path: 'list',                  component: Components.PostsHome }, 
+  {name:'posts.daily',    path:'/',                 	component: Components.PostsDaily}, // index route
+  {name:'posts.single',   path:'posts/:_id(/:slug)',    component: Components.PostsSingle},
+  {name:'users.single',   path:'users/:slug',           component: Components.UsersSingle},
+  {name:'users.account',  path:'account',               component: Components.UsersAccount},
+  {name:'resetPassword',  path:'reset-password/:token', component: Components.UsersResetPassword},
+  {name:'users.edit',     path:'users/:slug/edit',      component: Components.UsersAccount},
+>>>>>>> Set daily as default route
 ]);
